f4af87cc5d8ba66ac75ab1a992231e7891852915
jean-louis PONS <jeanlouis.stri@gmail.com>
2021-03-22 18:26:58 +0100

    fix rmi Naming on main Server

--- src/serveur/MainServeur.java
+++ src/serveur/MainServeur.java
@@ -3,10 +3,11 @@ package serveur;
 import java.net.MalformedURLException;
 import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
-import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+
+import javax.naming.NameAlreadyBoundException;
 
 /**
  * Classe qui crée un jeu et le met à disposition via RMI
@@ -20,23 +21,29 @@ public class MainServeur {
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[] args) throws RemoteException, MalformedURLException, AlreadyBoundException {
-		//System.setSecurityManager(new RMISecurityManager());
+	public static void main(String[] args) throws RemoteException, MalformedURLException, AlreadyBoundException, NameAlreadyBoundException {
 		Registry registry = null;
 		// Réservation d'un port pour mettre a disposition l'objet jeu
-		try {
-			registry = LocateRegistry.getRegistry(1099);
-		} catch (RemoteException e) {
-			registry = LocateRegistry.createRegistry(1099);
-		}
+		
+		registry = LocateRegistry.createRegistry(1099);
+
 		// Instanciation de l'objet jeu possédant les méthodes à partager
 		EchecImpl echec = new EchecImpl();
-		// Liaison de l'objet jeu au port réservé
 		try {
 			Naming.bind("rmi://localhost:1099/Echec", echec);
+			System.out.println("Interface partagée");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (AlreadyBoundException e) {
-			Naming.rebind("rmi://localhost:1099/Echec", echec);
-		}	
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
